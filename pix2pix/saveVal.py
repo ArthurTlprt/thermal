e4aac8b1d0685031a77325c72d2843c1d889f818
Apalog <gaetane.jallais@isen-lille.fr>
2018-04-05 10:45:26 +0200

    saveVal

---
+++ pix2pix/saveVal.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+import os
+
+def copy():
+	size = 41640
+
+	for i in range(size):
+		A = Image.open("datasets/thermal2rgb/testA/"+str(i)+".jpg")
+		A.load()
+		B = Image.open("datasets/thermal2rgb/testB/"+str(i)+".jpg")
+		B.load()
+		dataA = np.array(A)
+		dataB = np.array(B)
+		arrayIm = np.concatenate((dataB,dataA),1)
+		im = Image.fromarray(arrayIm)
+		if i>size/2:
+			im.save("datasets/thermal2rgb/val/"+str(i)+".jpg", "JPEG")
+		elif i<size/2:
+			im.save("datasets/thermal2rgb/test/"+str(i)+".jpg", "JPEG")
+
+
+def train():
+	size = 50184
+
+	for i in range(size):
+		A = Image.open("datasets/thermal2rgb/trainA/"+str(i)+".jpg")
+		A.load()
+		B = Image.open("datasets/thermal2rgb/trainB/"+str(i)+".jpg")
+		B.load()
+		dataA = np.array(A)
+		dataB = np.array(B)
+		arrayIm = np.concatenate((dataB,dataA),1)
+		im = Image.fromarray(arrayIm)
+		im.save("datasets/thermal2rgb/train/"+str(i)+".jpg", "JPEG")
+
+
+
+train()
