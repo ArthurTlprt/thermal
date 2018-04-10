50367d3facdd7eb102a8ed1d3a41917ce3aa30ae
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2018-04-10 14:12:28 +0200

    push my wonderfull code

--- GAN/pix2pix/evaluate.py
+++ GAN/pix2pix/evaluate.py
@@ -7,6 +7,7 @@ from skimage.measure import compare_ssim as ssim
 from keras.models import load_model
 from data_loader import DataLoader
 
+from skimage.measure import compare_ssim as ssim
 
 generator = load_model("models/13.1069565564inv.h5")
 
@@ -18,8 +19,17 @@ data_loader = DataLoader(dataset_name=dataset_name, img_res=(img_rows, img_cols)
 imgs_A, imgs_B = data_loader.load_data(batch_size=2, is_testing=True)
 fake_A = generator.predict(imgs_B)
 
-print(fake_A.shape)
 
+fake_A = np.resize(fake_A,(128,128,3))
+imgs_B = np.resize(imgs_B,(128,128,3))
+fake_A = np.asarray(fake_A,dtype='float')
+imgs_B = np.asarray(imgs_B,dtype='float')
+m_A = mse(imgs_B,fake_A)
+s_A = ssim(imgs_B, fake_A, multichannel=True)
+print("MSE: "+str(m_A)+" SSIM: "+str(s_A))
+
+
+print(fake_A.shape)
 
 gen_imgs = np.concatenate([imgs_B, fake_A, imgs_A])
 
--- GAN/pix2pix/pix2pix.py
+++ GAN/pix2pix/pix2pix.py
@@ -20,6 +20,12 @@ from skimage import data, img_as_float
 from skimage.measure import compare_ssim as ssim
 import cv2
 
+def mse(imageA,imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    # /!\ Images needs same dimensions
+    # divise by number of pixels
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
 
 class Pix2Pix():
     def __init__(self):
@@ -203,11 +209,11 @@ class Pix2Pix():
         gen_imgs = np.concatenate([imgs_B, fake_A, imgs_A])
 
         fake_A = np.resize(fake_A,(128,128,3))
-        imgs_A = np.resize(imgs_A,(128,128,3))
+        imgs_B = np.resize(imgs_B,(128,128,3))
         fake_A = np.asarray(fake_A,dtype='float')
-        imgs_A = np.asarray(imgs_A,dtype='float')
-        m_A = mse(imgs_A,fake_A)
-        s_A = ssim(imgs_A, fake_A, multichannel=True)
+        imgs_B = np.asarray(imgs_B,dtype='float')
+        m_A = mse(imgs_B,fake_A)
+        s_A = ssim(imgs_B, fake_A, multichannel=True)
         print("MSE: "+str(m_A)+" SSIM: "+str(s_A))
 
         # Rescale images 0 - 1
@@ -225,14 +231,8 @@ class Pix2Pix():
         fig.savefig("images/thermal2rgbinv/%dinv.png" % (epoch))
         plt.close()
 
-    def mse(imageA,imageB): 
-        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-        # /!\ Images needs same dimensions
-        # divise by number of pixels
-        err /= float(imageA.shape[0] * imageA.shape[1])
-        return err
 
 
 if __name__ == '__main__':
     gan = Pix2Pix()
-    gan.train(epochs=30000, batch_size=4, save_interval=200)
+    gan.train(epochs=30000, batch_size=4, save_interval=10)
