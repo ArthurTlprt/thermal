0dad83ebd6a5e745137dc00c7a282a2640e9a5be
Apalog <gaetane.jallais@isen-lille.fr>
2018-04-11 11:35:37 +0200

    evaluate cy

--- GAN/cycle-gan/evaluate.py
+++ GAN/cycle-gan/evaluate.py
@@ -10,11 +10,12 @@ from keras.models import load_model
 from data_loader import DataLoader
 
 from skimage.measure import compare_ssim as ssim
+import matplotlib.pyplot as plt
 
 
 n = int(sys.argv[1])
 print(n)
-generator = load_model("models/20.7344712317.h5")
+generator = load_model("models/AB/5.38045740128.h5")
 
 def mse(x, y):
     return np.linalg.norm(x - y)
@@ -33,14 +34,13 @@ img_cols = 128
 dataset_name = 'thermal2rgb'
 data_loader = DataLoader(dataset_name=dataset_name, img_res=(img_rows, img_cols))
 
-
-
 for j in range(n//10):
     imgs_A, imgs_B = data_loader.load_data(batch_size=10, is_testing=True)
     fakes_A = generator.predict(imgs_B)
     fakes_A = np.asarray(fakes_A,dtype=np.float64)
-    fakes_B = generator.predict(imgs_A)
-    fakes_B = np.asarray(fakes_b,dtype=np.float64)
+    plt.imshow(imgs_A)
+    plt.show()
+    plt.imshow(fakes_A)
 
     for i, img_A in enumerate(imgs_A):
         ac = ssim(fakes_A[i], img_A, data_range=img_A.max() - img_A.min(), multichannel=True)
@@ -48,14 +48,7 @@ for j in range(n//10):
 
         err = mse(img_A,fakes_A[i])
         error_mse.append(err)
-    for i, img_B in enumerate(imgs_B):
-        ac = ssim(fakes_B[i],img_B, data_range=img_A.max() - img_B.min(), multichannel=True)
-        accuracy_ssim_B.append(ac)
-
-        err = mse(img_B,fakes_A[i])
-        error_mse_B.append(err)
 
 
 
-print("RGB à thermique : ssim -"+mean(accuracy_ssim_A)+" mse -"+mean(error_mse_A))
-print("Thermique à RGB : ssim -"+mean(accuracy_ssim_B)+" mse -"+mean(error_mse_B))
+print("Thermique à RGB : ssim -"+mean(accuracy_ssim_A)+" mse -"+mean(error_mse_A))
